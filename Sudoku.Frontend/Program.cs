575d17c71f0b25c388374e377003b4155313c4f6
Chris <chris-schilders@hotmail.nl>
2021-04-19 18:39:51 +0200

    Initial commit

---
+++ Sudoku.Frontend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sudoku.Frontend
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
