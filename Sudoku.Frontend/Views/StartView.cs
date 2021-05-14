4d388e1f2b490f8a80b70f9b870194499a847833
Chris <chris-schilders@hotmail.nl>
2021-05-14 02:18:11 +0200

    Cleanup start view

--- Sudoku.Frontend/Views/StartView.cs
+++ Sudoku.Frontend/Views/StartView.cs
@@ -14,23 +14,23 @@ namespace Sudoku.Frontend.Views
             Console.OutputEncoding = Encoding.UTF8;
             
             _model = model;
-            Console.WriteLine("Welcome to Sudoku.\n\n");
+            Console.WriteLine("Welcome to Sudoku.\n");
         }
         
         public void Update()
         {
             if (_model.SudokuPath == null)
             {
-                Console.Write("Please enter a sudoku file path:");
+                Console.Write("Please enter a sudoku file path: ");
                 _model.SudokuPath = Console.ReadLine();
             }
             
             Console.WriteLine("\nSettings:");
-            Console.WriteLine($" - DisplayMode: {(_model.SimpleDisplay ? "Simple" : "Advanced" )}");
-            Console.WriteLine($" - File Path: {_model.SudokuPath ?? "None"}");
+            Console.WriteLine($" - Display Mode: {(_model.SimpleDisplay ? "Simple" : "Advanced" )}");
+            Console.WriteLine($" - File Path: '{_model.SudokuPath ?? "None"}'");
 
             Console.WriteLine("\nOptions:");
-            Console.WriteLine(" - To change the display mode press 'D'");
+            Console.WriteLine(" - To toggle the display mode press 'D'");
             Console.WriteLine(" - To change the file path mode press 'F'");
             Console.WriteLine(" - Press 'S' to start the game");
         }
