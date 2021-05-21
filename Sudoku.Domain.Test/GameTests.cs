e62c175a13eb397be468b1e84d97415cf9a6400f
Chris <chris-schilders@hotmail.nl>
2021-05-21 17:04:40 +0200

    Updated remove replace and place tests

--- Sudoku.Domain.Test/GameTests.cs
+++ Sudoku.Domain.Test/GameTests.cs
@@ -54,6 +54,29 @@ namespace Sudoku.Domain.Test
         }
         
         [Theory]
+        [InlineData(1, 3, 1, 2, "./Resources/empty.samurai")]
+        [InlineData(2, 4, 2, 3, "./Resources/empty.samurai")]
+        [InlineData(3, 5, 3, 4, "./Resources/empty.samurai")]
+        [InlineData(4, 6, 4, 5, "./Resources/empty.samurai")]
+        [InlineData(12, 7, 4, 5, "./Resources/empty.samurai")]
+        [InlineData(13, 8, 4, 5, "./Resources/empty.samurai")]
+        [InlineData(14, 6, 4, 5, "./Resources/empty.samurai")]
+        [InlineData(1, 3, 1, 2, "./Resources/empty.4x4")]
+        [InlineData(2, 4, 2, 3, "./Resources/empty.9x9")]
+        [InlineData(3, 5, 3, 4, "./Resources/empty.6x6")]
+        public void Should_Replace_Number_On_Puzzle(int x, int y, int number, int replace, string path)
+        {
+            // Arrange
+            var game = new GameReader().Read(path);
+
+            // Act & Assert
+            Assert.True(game.Place(new Point(x, y), number));
+            Assert.True(game.Place(new Point(x, y), replace));
+            Assert.Equal(replace, game.Cells[y, x].Definite);
+        }
+        
+        
+        [Theory]
         [InlineData(1, 3, 1, "./Resources/empty.samurai")]
         [InlineData(2, 4, 2, "./Resources/empty.samurai")]
         [InlineData(3, 5, 3, "./Resources/empty.samurai")]
