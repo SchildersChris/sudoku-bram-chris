d9386b0ddb6162b144e5be6a4ff5f88c8c1c1b9d
Chris <chris-schilders@hotmail.nl>
2021-06-01 13:35:10 +0200

    Added check of placement

--- Sudoku.Domain/Composite/CellLeaf.cs
+++ Sudoku.Domain/Composite/CellLeaf.cs
@@ -46,7 +46,10 @@ namespace Sudoku.Domain.Composite
 
         public void Layout(ICell[,] cells)
         {
-            if (_point.X < cells.GetLength(1) && _point.Y < cells.GetLength(0))
+            if (_point.X >= cells.GetLength(1) || _point.Y >= cells.GetLength(0)) 
+                return;
+            
+            if (cells[_point.Y, _point.X].Definite < Definite)
             {
                 cells[_point.Y, _point.X] = this;
             }
