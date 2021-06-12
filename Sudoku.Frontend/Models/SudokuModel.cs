731edc930d9fadb723330ae08bea9531c4664fb1
Chris <chris-schilders@hotmail.nl>
2021-06-12 20:34:43 +0200

    Cleanup and removed cell model

--- Sudoku.Frontend/Models/SudokuModel.cs
+++ Sudoku.Frontend/Models/SudokuModel.cs
@@ -7,28 +7,13 @@ namespace Sudoku.Frontend.Models
 {
     public class SudokuModel
     {
-
-        private readonly ICell[,] _cells;
-        public CellModel[,] Cells { get; }
+        public ICell[,] Cells { get; }
         public EditorState State { get; set; }
         public Point Position { get; private set; }
         
         public SudokuModel(ICell[,] cells, EditorState state)
         {
-            _cells = cells;
-            Cells = new CellModel[_cells.GetHeight(), _cells.GetWidth()];
-            for (var y = 0; y < _cells.GetHeight(); y++)
-            {
-                for (var x = 0; x < _cells.GetWidth(); x++)
-                {
-                    var cell = _cells.Get(x, y);
-                    if (cell != null)
-                    {
-                        Cells.Set(x, y, new CellModel(cell));
-                    }
-                }
-            }
-            
+            Cells = cells;
             State = state;
             Position = new Point(0, 0);
         }
@@ -36,7 +21,7 @@ namespace Sudoku.Frontend.Models
         public void Move(Size size)
         {
             var newPos = Position + size;
-            if (_cells.Contains(newPos) && _cells.Get(newPos) != null)
+            if (Cells.Contains(newPos) && Cells.Get(newPos) != null)
             {
                 Position = newPos;
             }
--- Sudoku.Frontend/Views/SudokuView.cs
+++ Sudoku.Frontend/Views/SudokuView.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Text;
 using Pastel;
 using Sudoku.Common.Extensions;
+using Sudoku.Domain.Composite.Interfaces;
 using Sudoku.Domain.Enums;
 using Sudoku.Frontend.Models;
 
@@ -25,12 +26,9 @@ namespace Sudoku.Frontend.Views
         {
             Console.Clear();
             Console.CursorVisible = false;
-            
-            var buffer = 
-                _model.State == EditorState.DefinitiveNumbers
-                ? CreateDefiniteView()
-                : CreateAuxiliaryView();
-            
+
+            var buffer = CreateSudoku();
+
             for (var y = 0; y < buffer.GetHeight(); y++)
             {
                 for (var x = 0; x < buffer.GetWidth(); x++)
@@ -44,14 +42,20 @@ namespace Sudoku.Frontend.Views
         }
 
 
-        private string[,] CreateDefiniteView()
+        private string[,] CreateSudoku()
         {
             var cells = _model.Cells;
             var height = cells.GetHeight();
             var width = cells.GetWidth();
+
+            int col = 1, row = 1;
+            if (_model.State == EditorState.AuxiliaryNumbers)
+            {
+                 (col, row) = width.Factorize();
+            }
             
-            var bufferHeight = height * 2 - 1;
-            var bufferWidth = width * 2 - 1;
+            var bufferHeight = height * row * 2 - 1;
+            var bufferWidth = width * col * 2 - 1;
             var buffer = new string[bufferHeight, bufferWidth];
 
             for (var y = 0; y < height; y++)
@@ -60,10 +64,17 @@ namespace Sudoku.Frontend.Views
                 {
                     var c = cells.Get(x, y);
             
-                    var y2 = y * 2;
-                    var x2 = x * 2;
-                    
-                    buffer.Set(x2, y2, c?.ToString() ?? " ");
+                    var y2 = y * row * 2;
+                    var x2 = x * col * 2;
+
+                    if (_model.State == EditorState.DefinitiveNumbers)
+                    {
+                        WriteDefiniteCell(c, buffer, x2, y2);
+                    }
+                    else
+                    {
+                        WriteAuxiliaryCell(c, buffer, x2, y2);
+                    }
 
                     if (c == null)
                     {
@@ -97,64 +108,30 @@ namespace Sudoku.Frontend.Views
             return buffer;
         }
         
-        private string[,] CreateAuxiliaryView()
+        private static void WriteDefiniteCell(ICell cell, string[,] buffer, int xStart, int yStart)
         {
-             var cells = _model.Cells;
-            var height = cells.GetHeight();
-            var width = cells.GetWidth();
-            var (col, row) = width.Factorize();
-
-            var bufferHeight = height * height * 2 - 1;
-            var bufferWidth = width * width * 2 - 1;
-            var buffer = new string[bufferHeight, bufferWidth];
-
-            for (var y = 0; y < height; y++)
+            if (cell == null)
             {
-                for (var x = 0; x < width; x++)
-                {
-                    var c = cells.Get(x, y);
-            
-                    var y2 = y * y * 2;
-                    var x2 = x * x * 2;
-                   
-                    for (var i = y2; i < row; i++)
-                    {
-                        for (var j = x2; j < col; j++)
-                        {
-                            buffer.Set(x2, y2, c?.ToString() ?? " ");
-                        }
-                    }
-                    
-                    if (c == null)
-                    {
-                        continue;
-                    }
-                    
-                    // Check for horizontal border
-                    if (cells.Contains(x + 1, y) && 
-                        cells.Get(x + 1, y) != null && 
-                        c.GridNumber != cells.Get(x + 1, y).GridNumber)
-                    {
-                        buffer.Set(x2 + 1, y2, "|".Pastel(Color.OrangeRed));
-                    }
-            
-                    // Check for vertical border
-                    if (cells.Contains(x, y + 1) && 
-                        cells.Get(x, y + 1) != null &&
-                        c.GridNumber != cells.Get(x, y + 1).GridNumber)
-                    {
-                        buffer.Set(x2, y2 + 1, "-".Pastel(Color.OrangeRed));
-                    }
+                buffer.Set(xStart, yStart, " ");
+                return;
+            }
 
-                    // Selector background
-                    if (_model.Position.X == x && _model.Position.Y == y)
-                    {
-                        buffer.Set(x2, y2, buffer.Get(x2, y2).PastelBg(Color.DarkGray).Pastel(Color.Black));
-                    }
-                }
+            var val = cell.Definite != 0 ? cell.Definite.ToString() : ".";
+            if (cell.Error == true)
+            {
+                val = val.Pastel(Color.White).PastelBg(Color.Red);
             }
             
-            return buffer;
+            buffer.Set(xStart, yStart, val);
+        }
+        
+        private static void WriteAuxiliaryCell(ICell cell, string[,] buffer, int xStart, int yStart)
+        {
+            var val = cell.Definite != 0 ? cell.Definite.ToString() : ".";
+            if (cell.Error == true)
+            {
+                val = val.Pastel(Color.White).PastelBg(Color.Red);
+            }
         }
     }
 }
